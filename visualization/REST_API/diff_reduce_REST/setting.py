e135e68edf0d100340baa10b2137a9ee169412fb
Luyu Liu <liuluyu0378@gmail.com>
2019-06-04 23:24:15 -0400

    Edits on draft

--- visualization/REST_API/diff_reduce_REST/setting.py
+++ visualization/REST_API/diff_reduce_REST/setting.py
@@ -3,8 +3,8 @@ DOMAIN = {
         '2_stops_risk_averse': {'datasource': {'source': '2_stops_risk_averse'}},
         '-2_stops_max': {'datasource': {'source': '-2_stops_max'}},
         '-2_stops': {'datasource': {'source': '-2_stops'}},
-        '2_stops': {'datasource': {'source': '2_stops'}},
         '2_stops_max': {'datasource': {'source': '2_stops_max'}},
+        '2_stops': {'datasource': {'source': '2_stops'}},
         }
 MONGO_HOST = 'localhost'
 MONGO_PORT = 27017
