6c3fd6f677aee690108d3912184b4ead986111ed
Luyu Liu <liuluyu0378@gmail.com>
2019-06-15 14:53:27 -0400

    ER min

---
+++ scr/ER/ER_min.py
@@ -0,0 +1,63 @@
+from pymongo import MongoClient
+from datetime import timedelta, date
+import datetime
+import multiprocessing
+import time
+
+import os, sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+import transfer_tools
+
+
+client = MongoClient('mongodb://localhost:27017/')
+
+db_GTFS = client.cota_gtfs
+db_real_time = client.cota_real_time
+db_trip_update = client.trip_update
+db_opt_result = client.cota_er
+
+def analyze_transfer(start_date, end_date):
+    records_dic={} # Avoid IO. But could be bad for small memory.
+    for single_date in transfer_tools.daterange(start_date, end_date):
+        
+        today_date = single_date.strftime("%Y%m%d")  # date
+        
+        today_seconds = time.mktime(time.strptime(today_date, "%Y%m%d"))
+
+        col_real_time = db_real_time["R"+today_date]
+        result_real_time = list(col_real_time.find({}))
+        col_er = db_opt_result["er_min"]
+        for each_record in result_real_time:
+            trip_id = each_record["trip_id"]
+            stop_id = each_record["stop_id"]
+
+            try:
+                records_dic[trip_id]
+            except:
+                records_dic[trip_id] = {}
+            else:
+                pass
+            
+            try:
+                records_dic[trip_id][stop_id]
+            except:
+                records_dic[trip_id][stop_id] = {}
+                records_dic[trip_id][stop_id]["trip_id"] = trip_id
+                records_dic[trip_id][stop_id]["stop_id"] = stop_id
+                records_dic[trip_id][stop_id]["time"] = each_record["time"] - today_seconds
+                records_dic[trip_id][stop_id]["count"] = 1
+            else:
+                records_dic[trip_id][stop_id]["time"] = min(records_dic[trip_id][stop_id]["time"], each_record["time"] - today_seconds)
+                records_dic[trip_id][stop_id]["count"] += 1
+            
+        print(today_date +" - Computation.")
+
+    for trip_id, trip_records in records_dic.items():
+        for stop_id, stop_time_records in trip_records.items():
+            col_er.insert_one(stop_time_records)
+
+    print(today_date +" - Database insert.")   
+
+
+if __name__ == '__main__':
+    analyze_transfer(date(2018, 2, 1), date(2019, 1, 31))
---
+++ scr/ER/ER_validate_min.py
@@ -0,0 +1,66 @@
+from pymongo import MongoClient
+from datetime import timedelta, date
+import datetime
+import multiprocessing
+
+import os, sys, time
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+import transfer_tools
+
+
+client = MongoClient('mongodb://localhost:27017/')
+
+db_GTFS = client.cota_gtfs
+db_real_time = client.cota_real_time
+db_trip_update = client.trip_update
+db_er = client.cota_er
+col_er = db_er.er_min
+
+criteria = 5
+
+
+def validate_er(single_date):
+    records_dic={} # Avoid IO. But could be bad for small memory.
+    today_date = single_date.strftime("%Y%m%d")  # date
+    today_date = single_date.strftime("%Y%m%d")  # date
+    today_seconds = time.mktime(time.strptime(today_date, "%Y%m%d"))
+    print(today_date + " - Start.")
+    col_real_time = db_real_time["R" + today_date]
+    rl_real_time = list(col_real_time.find({}))
+    col_er_val = db_er["min_"+today_date]
+    total_count = len(rl_real_time)
+    count = 0
+
+    for each_record in rl_real_time:
+        trip_id = each_record["trip_id"]
+        stop_id = each_record["stop_id"]
+        route_id = each_record["route_id"]
+        real_time = each_record["time"]
+        query_rl = col_er.find_one({"trip_id": trip_id, "stop_id": stop_id})
+        if query_rl == None:
+            continue
+        each_record["time_er_arr"] = int(query_rl["time"] + today_seconds)
+        alt_cal_rl = transfer_tools.find_alt_time(each_record["time_er_arr"], route_id, stop_id, today_date, criteria)
+        each_record["time_er_alt"] = alt_cal_rl[0]
+        each_record["time_er_trip_id"] = alt_cal_rl[1]
+        each_record["time_er_trip_sequence"] = alt_cal_rl[2]
+        each_record.pop("_id", None)
+        col_er_val.insert_one(each_record)
+
+        count+=1
+    
+    print(today_date + " - Done.")
+
+if __name__ == "__main__":
+    # analyze_transfer(date(2018, 2, 2))
+    start_date = date(2018, 2, 1)
+    end_date = date(2019, 1, 31)
+
+    # validate_er(start_date)
+    cores = int(multiprocessing.cpu_count()/4*3)
+    pool = multiprocessing.Pool(processes=cores)
+    date_range = transfer_tools.daterange(start_date, end_date)
+    output = []
+    output = pool.map(validate_er, date_range)
+    pool.close()
+    pool.join()
